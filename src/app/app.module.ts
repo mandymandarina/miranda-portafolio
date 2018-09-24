addb46f77f6da5f08ca25f9137e834e8996914ab
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-09-24 13:46:20 -0300

    ambiente de desarrollo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FirstScreenComponent } from './first-screen/first-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
