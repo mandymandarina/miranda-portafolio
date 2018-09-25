c6ca4ec55135a3bac51fdc3bc08a14e509f8cf60
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-09-24 22:36:02 -0300

    rutas listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,48 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FirstScreenComponent } from './first-screen/first-screen.component';
+import { ProjectComponent } from './project/project.component';
+import { MenuComponent } from './menu/menu.component';
+import { FirstNameComponent } from './first-name/first-name.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil/perfil.component';
+import { ContacComponent } from './contac/contac.component';
 
+const appRoutes: Routes = [
+  {
+    path:'',
+    component: FirstScreenComponent 
+   },
+   {
+    path:'perfil',
+    component: PerfilComponent  
+  },
+  {
+    path:'project',
+    component: ProjectComponent 
+  },
+  {
+    path:'contac',
+    component: ContacComponent  
+  },
+  {
+    path:'home',
+    component: FirstScreenComponent
+  },
+]
 @NgModule({
   declarations: [
     AppComponent,
-    FirstScreenComponent
+    FirstScreenComponent,
+    ProjectComponent,
+    MenuComponent,
+    FirstNameComponent,
+    PerfilComponent,
+    ContacComponent,    
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule
   ],
   providers: [],
