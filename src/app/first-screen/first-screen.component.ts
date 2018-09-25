f7dced803ac7cd6625ec56f31ee0955c6bc7db5a
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-09-25 09:52:57 -0300

    se instalo animaciones

--- src/app/first-screen/first-screen.component.ts
+++ src/app/first-screen/first-screen.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounce } from 'ng-animate';
 @Component({
   selector: 'app-first-screen',
   templateUrl: './first-screen.component.html',
-  styleUrls: ['./first-screen.component.css']
+  styleUrls: ['./first-screen.component.css'],
+  animations: [
+    trigger('bounce',[transition('*=>*',useAnimation(bounce))])
+  ]
 })
 export class FirstScreenComponent implements OnInit {
-
+bounce:any;
   constructor() { }
 
   ngOnInit() {
